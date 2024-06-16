5a82bf30a51a07f2e70fe3dcf5b3bf28306a8dc5
zearr11 <cesar_gamarra_al@hotmail.com>
2024-06-15 22:44:39 -0500

    *Se actualiza*

--- ejercicio.py
+++ ejercicio.py
@@ -1,1 +1,32 @@
-print("Hola mundo!")
+print()
+
+c = False
+
+while True:
+    try:
+        v1 = int(input("Ingresa solo un numero entero: "))
+        v2 = int(input("Ingresa otro numero entero: "))
+
+        print(v1 / v2)
+    
+        c = True
+
+        break
+    
+    except ValueError:
+        print("Solo se aceptan numeros enteros..")
+    
+    except ZeroDivisionError:
+        print("No se puede dividir por cero....")
+        
+    else:
+        print("HOLA")
+        print()
+    
+    finally:
+        if c == True:
+            print("Finalizando programa")
+        else:
+            print("Intenta nuevamente........")
+    
+    
